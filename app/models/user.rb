bc06832de5d9b85cecadd2f782f9ea085978014d
kliprift <eddie@worldone.com.my>
2016-08-04 11:03:14 +0800

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+	# This is Sinatra! Remember to create a migration!
+  attr_accessor :password_confirmation
+  EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates :name, presence: true
+  validates :email, :presence => true, uniqueness: {case_sensitive: false, message: " was used before"}, :format => EMAIL_REGEX
+  validates :password, :confirmation => true 
+  has_many :tweets  
+  has_many :followers 
+ 
+end
