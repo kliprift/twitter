313d2faee4fca2e5bdbf3b79607d03c43b1065b9
kliprift <eddie@worldone.com.my>
2016-08-04 12:52:46 +0800

    added user view, controller, helper method,  sign in and signup working, css and static index modification

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+#post form for user signup
+post '/users/signup' do
+  @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to "/users/login"
+    else
+      @errors = @user.errors.full_messages
+      erb :"static/index"
+    end
+end
+
+post '/users/login' do
+  @user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+    if @user # @user is found
+      session[:user_id] = @user.id
+      redirect to "/users/login"
+    else # @user = nil, not found
+      @errors = "Please check your input"
+      erb :"static/index"
+    end
+  end
+
+get '/users/login' do
+  @user = current_user
+    if @user
+      session[:user_id] = @user.id
+      erb :"users/index"
+    else
+      erb :"static/index"
+    end
+  end  
+
+
+get '/signout' do
+  session[:user_id] = nil
+  erb :"static/index"
+end 
+
+
+#post form for user signup
+
+
+
+
